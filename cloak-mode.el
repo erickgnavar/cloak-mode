61da7c6710ec716ddb99c9adb7e5156fd1a16335
Erick Navarro <erick@navarro.io>
2022-10-09 19:11:53 -0500

    Improve commentary section

--- cloak-mode.el
+++ cloak-mode.el
@@ -8,6 +8,9 @@
 ;; Package-Requires: ((emacs "27.1"))
 
 ;;; Commentary:
+;; Hide sensitive using pre defined regex patterns per major mode.
+;; It allows to define specific regex per each major mode where we want to hide
+;; sensitive data
 
 ;; Usage: For example if we want to add it to envrc files
 ;;   (require 'cloak-mode)
